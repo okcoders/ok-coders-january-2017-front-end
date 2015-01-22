b30fb68dcfa6c823d342562949641f71e8aa4243
Joseph Asante <josephnasante@yahoo.com>
2015-01-22 16:15:27 -0600

    Did the homework

--- 10-node/10-exercises/template/printer.js
+++ 10-node/10-exercises/template/printer.js
@@ -2,3 +2,19 @@
 // add file read and print code
 // wrap it up in a function
 // export that function as `print`
+
+var fs = require('fs');
+
+module.exports = { 
+	print: function(path) {
+		fs.readFile(path, function(err, data) {
+		
+			if (err) { // Always do error checking first!
+				// sad
+			} else {
+				console.log('File contents:');
+				console.log(data.toString());
+			}
+		});
+	}
+}
--- 10-node/10-exercises/template/reader.js
+++ 10-node/10-exercises/template/reader.js
@@ -1,5 +1,13 @@
 
 var fs = require('fs');
 
-// ...
-// and much more
+var path = process.argv[2];
+
+fs.readFile(path, function(err, data) {
+	if (err) { // Always do error checking first!
+		// sad
+	} else {
+		console.log('File contents:');
+		console.log(data.toString());
+	}
+});
